b179203cf0d4ad891bc7ee95e8af6e4d8866c7e4
Natalia Volkova <natalia.volkofff@gmail.com>
2020-01-21 21:00:37 +0300

    feat: add mobX provider and inject

--- base-init/src/components/Stream/index.js
+++ base-init/src/components/Stream/index.js
@@ -1,22 +1,25 @@
 import React from "react";
-import { observer } from "mobx-react";
+import { observer, inject } from "mobx-react";
 import Stream from "./presenter";
 import { CLIENT_ID } from "../../constants/auth";
 import { auth } from "../../actions/auth";
-import userStore from "../../stores/userStore";
-import trackStore from "../../stores/trackStore";
 
-const StreamContainer = observer(() => {
-  return (
-    <Stream
-      me={userStore.me}
-      tracks={trackStore.tracks}
-      activeTrack={trackStore.activeTrack}
-      clientId={CLIENT_ID}
-      onAuth={auth}
-      onPlay={trackStore.onPlay}
-    />
-  );
-});
+const StreamContainer = inject(
+  "userStore",
+  "trackStore"
+)(
+  observer(({ userStore, trackStore }) => {
+    return (
+      <Stream
+        me={userStore.me}
+        tracks={trackStore.tracks}
+        activeTrack={trackStore.activeTrack}
+        clientId={CLIENT_ID}
+        onAuth={auth}
+        onPlay={trackStore.onPlay}
+      />
+    );
+  })
+);
 
 export default StreamContainer;
--- base-init/src/index.js
+++ base-init/src/index.js
@@ -1,11 +1,14 @@
 import SC from "soundcloud";
+import { Provider } from "mobx-react";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Router, Route, IndexRoute, browserHistory } from "react-router";
-import { configure } from 'mobx';
+import { configure } from "mobx";
 import App from "./components/App";
 import Callback from "./components/Callback";
 import Stream from "./components/Stream";
+import userStore from "./stores/userStore";
+import trackStore from "./stores/trackStore";
 import { CLIENT_ID, REDIRECT_URI } from "./constants/auth";
 
 SC.initialize({ client_id: CLIENT_ID, redirect_uri: REDIRECT_URI });
@@ -13,13 +16,17 @@ SC.initialize({ client_id: CLIENT_ID, redirect_uri: REDIRECT_URI });
 // to enable mutating the state from outside the stores
 configure({ enforceActions: true });
 
+const stores = { userStore, trackStore };
+
 ReactDOM.render(
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <IndexRoute component={Stream} />
-      <Route path="/" component={Stream} />
-      <Route path="/callback" component={Callback} />
-    </Route>
-  </Router>,
+  <Provider {...stores}>
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Stream} />
+        <Route path="/" component={Stream} />
+        <Route path="/callback" component={Callback} />
+      </Route>
+    </Router>
+  </Provider>,
   document.getElementById("app")
 );
