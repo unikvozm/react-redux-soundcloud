a5a8cbaa730d598f6b8b792fbe31a0c612f68415
Natalia Volkova <natalia.volkofff@gmail.com>
2020-01-21 19:28:29 +0300

    feat: add stores

---
+++ base-init/src/stores/userStore.js
@@ -0,0 +1,14 @@
+import { observable } from "mobx";
+
+class UserStore {
+  @observable me;
+
+  constructor() {
+    this.me = null;
+  }
+}
+
+const userStore = new UserStore();
+
+export default userStore;
+export { UserStore };
